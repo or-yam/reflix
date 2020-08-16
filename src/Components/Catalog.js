8a8bd22b8d828767c8819d13fb7f834e90a21574
Oryam <48219965+or-yam@users.noreply.github.com>
2020-08-16 13:55:31 +0300

    Catalog moved to functional component

--- src/Components/Catalog.js
+++ src/Components/Catalog.js
@@ -1,8 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import MovieCard from './MovieCard';
 import '../styles/Catalog.css';
 
+export default function Catalog(props) {
+  const { movies, rented, user, onSearchChange, updateRent } = props;
+
+  return (
+    <div>
+      <div className="catalog-container">
+        <nav className="search-nav">
+          <Link to="/">
+            <img
+              id="logo"
+              src="https://brand.netflix.com/static/assets/icons/netflix_logo.svg"
+              alt="logo"
+            />
+          </Link>
+          <input
+            type="search"
+            placeholder="Search for title..."
+            id="search-input"
+            onChange={onSearchChange}
+          />
+          <div className="user-budget">
+            <img src={user.img} alt="user" />
+            <span>{user.name}</span>
+            <span>Budget: {user.budget}$</span>
+          </div>
+        </nav>
+
+        <div className="rented-container">
+          {rented.length ? <h3>Rented</h3> : null}
+          <div className="movies-container">
+            {rented.map((m) =>
+              m ? (
+                <MovieCard key={m.id} updateRent={updateRent} movieData={m} />
+              ) : null
+            )}
+          </div>
+        </div>
+
+        <div className="catalog-container">
+          <h3>Catalog</h3>
+          <div className="movies-container">
+            {movies.map((m) => (
+              <MovieCard
+                key={m.id}
+                updateRent={updateRent}
+                movieData={m}
+                user={user}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*
 export default class Catalog extends Component {
   render() {
     const { movies, rented, user, onSearchChange, updateRent } = this.props;
@@ -58,3 +115,4 @@ export default class Catalog extends Component {
     );
   }
 }
+*/
