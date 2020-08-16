93476af8c9ec2d524d0b51708e8d32899cd059b5
Oryam <48219965+or-yam@users.noreply.github.com>
2020-08-16 22:06:51 +0300

    connected to external api with axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
 
 import movieCatalog from './data/MoviesCatalog';
 import dummyUsers from './data/dummyUsers';
@@ -9,7 +10,6 @@ import MoviePage from './Components/MoviePage';
 
 import './styles/App.css';
 
-
 export default function App() {
   const RENT_PRICE = 7;
   const users = dummyUsers;
@@ -17,6 +17,25 @@ export default function App() {
   const [moviesList, setMoviesList] = useState(movieCatalog);
   const [moviesRented, setMoviesRented] = useState([]);
   const [searchField, setSearchField] = useState('');
+
+  const API_KEY = '22fc91e4eaa31cd6b9aaa5f3a542def7';
+  const IMG_PREFIX = 'https://image.tmdb.org/t/p/w370_and_h556_bestv2';
+
+  useEffect(() => {
+    axios(
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+    ).then((res) => {
+      const list = res.data.results.map((m) => ({
+        id: m.id,
+        isRented: false,
+        title: m.title,
+        year: m.release_date,
+        descrShort: m.overview,
+        img: IMG_PREFIX + m.poster_path,
+      }));
+      setMoviesList(list);
+    });
+  }, []);
 
   const filteredMovies = moviesList.filter((movie) =>
     movie.title.toLowerCase().includes(searchField.toLocaleLowerCase())
